d73a39fc90647ed7007ff1bffe72a7839d2949dc
bethanvaughan <86553590+bethanvaughan@users.noreply.github.com>
2021-06-28 14:43:23 +0100

    Replaced previous h1 and email/password buttons with weather app html, css,and js

--- script.js
+++ script.js
@@ -1,1 +1,86 @@
-alert("Hello World ");
+let now = new Date();
+let h2 = document.querySelector("h2");
+
+let year = now.getFullYear();
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+h2.innerHTML = `${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", displayCity);
+
+function searchLocation(position) {
+  let apiKey = "8774a29a1f236853c07b68b1cbf32995";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function displayCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+function displayWeatherConditions(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature-city").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#temp-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function displayCity(event) {
+  event.preventDefault();
+  let searchBoxInput = document.querySelector("#search-text-input");
+  let apiKey = "8774a29a1f236853c07b68b1cbf32995";
+  let city = document.querySelector("#search-text-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  console.log(axios);
+  axios.get(apiUrl).then(displayWeatherConditions);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchBoxInput.value}`;
+}
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", displayCurrentLocation);
